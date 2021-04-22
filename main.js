2faf8b99a138f88be57500fe9f6257d364919298
k.lala <klodian.lala@gmx.de>
2021-04-22 10:01:19 +0200

    multiple changes

--- main.js
+++ main.js
@@ -1,12 +1,69 @@
-import menu from './menu.css';
-import cards from './src/frontpage/cards.html';
-import memberCards from './src/frontpage/memberCards.html';
-import news from './src/frontpage/news.html'
-const app = document.getElementById('app');
+// frontpage cards
+const cards = document.createElement('object');
+    cards.data = './src/frontpage/cards.html';
+    cards.rel = 'text/html';
+    cards.style = 'width: 100%;';
+    cards.className ='cards';
+const memberCards = document.createElement('object');
+    memberCards.data = ('./src/frontpage/memberCards.html');
+    memberCards.rel = 'text/html';
+    memberCards.style = 'width: 100%;';
+    memberCards.className ='memberCards';
+const news = document.createElement('object');
+    news.data = ('./src/frontpage/news.html');
+    news.rel = 'text/html';
+    news.style = 'width: 100%;';
+    news.className ='news';
 
-app.appendChild(cards);
-app.appendChild(news);
-app.appendChild(memberCards);
-// app.innerHTML += cards;
-// app.innerHTML += news;
-// app.innerHTML += memberCards;
+const styles = document.createElement('link');
+styles.href = './menu.css';
+styles.rel = 'stylesheet';
+// news page ----------------------------------------------------------
+import {mainAsk, simpleText} from './src/news/news.js'
+const stylesNews = document.createElement('link');
+stylesNews.href = './src/news/news.css';
+stylesNews.rel = 'stylesheet';
+const appNews = document.createElement("div");
+appNews.innerHTML += mainAsk();
+appNews.innerHTML += simpleText();
+appNews.appendChild(stylesNews);
+// end news page ------------------------------------------------------
+const app = document.getElementById('app');
+function displayHash() {
+    var theHash = window.location.hash;
+    if (theHash.length == 0) { theHash = "_index"; }
+    switch (theHash) {
+        case "_index":
+            // app.appendChild(cards);
+            // app.appendChild(news);
+            // app.appendChild(memberCards);
+            app.replaceChildren(cards, news, memberCards);
+            
+            break;
+        case "#news":
+            // console.log(newsSite);
+            app.replaceChildren(appNews);
+            break;
+        case "#img":
+            app.replaceChildren();
+            break;
+        case "#videos":
+            app.replaceChildren();
+            break;
+        default:
+            // app.replaceChildren(cards, news, memberCards);
+            break;
+    }
+    console.log("H",theHash);
+	  return true;
+	}
+
+	window.addEventListener("hashchange", function() {
+	  console.log("hashchange event");
+	  displayHash();
+	});
+
+	window.addEventListener("DOMContentLoaded", function(ev) {
+	  console.log("DOMContentLoaded event");
+	  displayHash();
+	});
