391681d48231d5180b200d04905ea3f8780b3ca6
Klodian Lala <k.lala@lalatech.de>
2021-07-03 09:49:45 +0200

    responsive news site

--- src/news/news.js
+++ src/news/news.js
@@ -1,18 +1,18 @@
 import dataNews from '../../data/newsData.js'
 // import news from './news.css';
-export function mainAsk()  {
-    return `
+export function mainAsk() {
+  return `
         <section>
         <center>
         <wide-cards imageInput="./src/news/skanderbeg.jpeg" backColor="#a9c0cb"
-        class="main-cards"
-        style="margin:21px;">
-        <h2 class="text h2" slot="title">Thirrje</h2>
-        <h3 class="text" slot="subtitle"></h3>
-        <p  class="text main-width" slot="text">
+        class="main-cards">
+        <h2 class="text news-text h2" slot="title">Thirrje</h2>
+        <h3 class="text news-text" slot="subtitle"></h3>
+        <p  class="text news-text main-width" slot="text">
             Jeto qytetin tend ndryshe!
             Na dergoni foto ose mesazhe pozitive ne inbox ta bejme kohen e qendrimit ne karantine me te bukur!
             Fotot dhe mesazhet do jene anonime!
+            <br />
             Keep calm and love Power of Education
         </p>
         </wide-cards>
@@ -21,8 +21,9 @@ export function mainAsk()  {
     `;
 
 }
-function onlyText(data, index){
-    return `
+
+function onlyText(data, index) {
+  return `
         <h1 class="news">${data.heading}</h1>
         <h3 class="news">${data.date}</h3>
         <p class="textNews news" onclick='${toggleSize()}'>
@@ -31,16 +32,16 @@ function onlyText(data, index){
         <p class="news">${data.fromPerson}</p>
     `;
 }
-const onlyImages = dataNews.reduceRight((acc, data, index) =>acc + onlyText(data, index), '');
-export function simpleText(){
-    return `<div>
+const onlyImages = dataNews.reduceRight((acc, data, index) => acc + onlyText(data, index), '');
+export function simpleText() {
+  return `<div>
         ${onlyImages}
     </div>
     `;
 
 }
-function toggleSize(evt){
-    return `event.currentTarget.classList.toggle("textToggle")
-    console.log(event)`
+
+function toggleSize(evt) {
+  return `event.currentTarget.classList.toggle("textToggle")`
 
 }
