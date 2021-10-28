3b15d02fff98b0a180fc8743cd0df591b6ffcb15
Klodian Lala <k.lala@lala-tech.de>
2021-10-28 17:56:47 +0200

    Add headers to backend

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 var express = require('express');
 var app = express();
-app.use(express.static("."));
-app.set('port',process.env.PORT || 3001);
+const helmet  = require('helmet');
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
+app.set('port',process.env.PORT || 3001);
+app.use(
+  helmet.referrerPolicy({
+    policy: "no-referrer"
+  })
+, helmet.noSniff()
+, helmet.frameguard({
+    action: "deny"
+  })
+, helmet.contentSecurityPolicy({
+    useDefaults: true
+  , directives: {
+      "default-src": "'self'"
+    }
+  })
+ );
+ app.use((req, res, next) => {
+  res.setHeader(
+    "Permissions-Policy",
+    "oversized-images=('self')"
+  );
+  next();
 });
+app.use(express.static("."));
 
 app.get('/',function(req,res){
-    res.redirect(path.join(__dirname, '/index.html'));
-    res.status(200).send(__dirname+"./");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.redirect(path.join(__dirname, '/index.html'));
+  res.status(200).send(__dirname+"./");
 });
 
 app.listen (app.get( 'port' ), function (){
